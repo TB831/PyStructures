79b206ffd38104637410b2fb16198777cb9c4aa6
TB831 <druu831@gmail.com>
2018-09-05 13:04:20 -0700

    anagrams complete

--- arrays/anagrams.py
+++ arrays/anagrams.py
@@ -1,20 +1,21 @@
 def anagram(s1, s2):
-    
+    '''
+    Given two strings, check to see if they are anagrams.
+    An anagram is when the two strings can be written using the
+    exact same letters (so you can just rearrange the letters to
+    get a different phrase or word).
+    '''
+    return sorted(s1) == sorted(s2)
+
+def cleanString(str):
+    '''
+    Function to clean a string
+    '''
+    clean = sorted(str.lower().replace(" ", ""))
+    return ''.join(clean)
 
 
 # Test Cases
-from nose.tools import assert_equal
-
-class AnagramTest(object):
-
-    def test(self,sol):
-        assert_equal(sol('go go go','gggooo'),True)
-        assert_equal(sol('abc','cba'),True)
-        assert_equal(sol('hi man','hi     man'),True)
-        assert_equal(sol('aabbcc','aabbc'),False)
-        assert_equal(sol('123','1 2'),False)
-        print "ALL TEST CASES PASSED"
-
-# Run Tests
-t = AnagramTest()
-t.test(anagram)
+anagram('dog','god')
+anagram('clint eastwood','old west action')
+anagram('aa','bb')
