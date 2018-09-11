b670eafc0c3f6310594bbfd48c1f67852c38e645
TB831 <druu831@gmail.com>
2018-09-11 15:41:04 -0700

    binarySearchRecursive complete

--- search/binarySerachRecursive.py
+++ search/binarySerachRecursive.py
@@ -1,7 +1,15 @@
 def binarSearchRecursive(arr, left, right, value):
-    '''
-    Recursive solution for binary search
-    '''
+    if (left > right): # Edge case
+        return False
 
-#Test Array
+    mid = int(left + ((right - left)/ 2))
+    if arr[mid] == value:
+        return True
+    elif value < arr[mid]:
+        return binarSearchRecursive(arr, left, mid - 1, value)
+    else:
+        return binarSearchRecursive(arr, mid + 1, right, value)
+
+#Test Arrays
 arr = [2, 3, 4, 10, 40]
+binarSearchRecursive(arr, 0, len(arr)-1, 3)
