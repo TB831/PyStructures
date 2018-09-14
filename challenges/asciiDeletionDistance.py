40b2f75827a5ca15cc2e0149dc5d8bd7168327f0
TB831 <druu831@gmail.com>
2018-09-14 09:45:57 -0700

    asciiDeletionDistance & binarySearchIterative

---
+++ challenges/asciiDeletionDistance.py
@@ -0,0 +1,7 @@
+    def minimumDeleteSum(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        
---
+++ search/binarySearchIterative.py
@@ -0,0 +1,53 @@
+def binarySearchIterative(arr, val):
+    l = 0
+    r = len(arr)-1
+
+    while l <= r:
+        mid = l + (r - l)/2
+        if arr[mid] == val:
+            return True
+        elif val < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return False
+
+arr = [2, 3, 4, 10, 40]
+binarySearchIterative(arr, 3)
+
+
+def binarySearch(arr, l, r, x):
+
+    while l <= r:
+
+        mid = l + (r - l)/2;
+
+        # Check if x is present at mid
+        if arr[mid] == x:
+            return mid
+
+        # If x is greater, ignore left half
+        elif arr[mid] < x:
+            l = mid + 1
+
+        # If x is smaller, ignore right half
+        else:
+            r = mid - 1
+
+    # If we reach here, then the element
+    # was not present
+    return -1
+
+
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+    print "Element is present at index %d" % result
+else:
+    print "Element is not present in array"
